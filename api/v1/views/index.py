d34d4ddeac687be3da6bda49cf9d73ab76b1093b
maggywairigu <wairigumaina2016@gmail.com>
2024-01-09 05:48:51 -0800

    task 1 files updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
+
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+@app_views.route("/status")
+def status():
+    '''
+        return JSON of OK status
+    '''
+    return jsonify({'status': 'OK'})
 
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
 
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStatus"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
