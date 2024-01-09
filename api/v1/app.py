fc733c0bcf9986eb6338a859225c564107d3ca8d
maggywairigu <wairigumaina2016@gmail.com>
2024-01-09 05:20:13 -0800

    documentation update

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,6 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
@@ -18,6 +17,7 @@ def teardown_appcontext(code):
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """error handler"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
